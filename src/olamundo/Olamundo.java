ecf0408f83bf95cb45be08a7793b02e5f9851fe9
rafael <rafael@rafael-Aspire-E1-531>
2017-07-27 00:05:57 -0300

    olamundo

---
+++ src/olamundo/Olamundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundo;
+
+/**
+ *
+ * @author rafael
+ */
+public class Olamundo{
+
+    
+    
+    
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+       
+    }
+    
+}
