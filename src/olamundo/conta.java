e585bd7be0f9d924f041c2e88d342fba72350b4e
brunobenet8 <bruno.rafaelbenet@gmail.com>
2017-07-27 00:09:02 -0300

    Update conta.java

--- src/olamundo/conta.java
+++ src/olamundo/conta.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package olamundo ;
 
-/**
- *
- * @author rafael
- */
 public class conta extends Thread{
     public void run(){
         for(int i = 0; i<= 10000; i++){
